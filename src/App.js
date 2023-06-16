919e82e3cb1ad4a56bf32fd2b585267781afeecc
ChahrazadBouragba <chbouragba25@gmail.com>
2023-06-16 20:07:37 +0100

    toggllefavorite

--- src/App.js
+++ src/App.js
@@ -11,8 +11,21 @@ import Images from './components/Images';
 import Post from './components/post';
 import Aesthetic from './components/Aesthetic';
 import Quotations from './components/Quotes';
+import favorite from './img/love.png'
+import heart from './img/full-heart.png'
+import link from './img/link.png'
+import { useState } from 'react';
 
 function App() {
+
+  connst [heartIcon, setheartIcon] = useState(false)
+
+  let heartIcon = heartIcon ? heart : favorite
+
+  function toggllefavorite() {
+
+  }
+
   return (
     <div className="Blog">
       <Aside/>
@@ -22,35 +35,73 @@ function App() {
 
           <div className="post--container">
             <Quotations numberOfQuotes={1}/>
+
+            <div className='reactions-div'>
+              <span className='reaction-container'>
+                <img className='reaction' src={link}></img>
+              </span>
+              
+              <span className='reaction-container'>
+                <img onClick={toggllefavorite} className='reaction' src={favorite}></img>
+              </span>
+            </div>
           </div>
 
           <div className="post--container">
             <Quotations numberOfQuotes={1}/>
+            <div className='reactions-div'>
+              <img className='reaction' src={link}></img>
+              <img className='reaction' src={favorite}></img>
+            </div>
           </div>
 
           <div className="post--container">
           <AestheticPicture  numberOfPictures={3}/>
+          <div className='reactions-div'>
+              <img className='reaction' src={link}></img>
+              <img className='reaction' src={favorite}></img>
+            </div>
           </div>
           
           <div className="post--container">
             <Quotations numberOfQuotes={1}/>
+            <div className='reactions-div'>
+              <img className='reaction' src={link}></img>
+              <img className='reaction' src={favorite}></img>
+            </div>
           </div>
 
           <div className="post--container">
             <AestheticPicture  numberOfPictures={6}/>
             <Quotations numberOfQuotes={1}/>
+            <div className='reactions-div'>
+              <img className='reaction' src={link}></img>
+              <img className='reaction' src={favorite}></img>
+            </div>
           </div>
 
           <div className="post--container">
             <AestheticPicture  numberOfPictures={6}/>
+            <div className='reactions-div'>
+              <img className='reaction' src={link}></img>
+              <img className='reaction' src={favorite}></img>
+            </div>
           </div>
 
           <div className="post--container">
             <Quotations numberOfQuotes={1}/>
+            <div className='reactions-div'>
+              <img className='reaction' src={link}></img>
+              <img className='reaction' src={favorite}></img>
+            </div>
           </div>
 
           <div className="post--container">
             <Images numberOfImages={5}/>
+            <div className='reactions-div'>
+              <img className='reaction' src={link}></img>
+              <img className='reaction' src={favorite}></img>
+            </div>
           </div>
 
           {/* <div className="post--container">
@@ -59,34 +110,62 @@ function App() {
 
           <div className="post--container">
             <Quotations numberOfQuotes={1}/>
+            <div className='reactions-div'>
+              <img className='reaction' src={link}></img>
+              <img className='reaction' src={favorite}></img>
+            </div>
           </div>
 
 
           <div className="post--container">
             <AestheticPicture  numberOfPictures={6}/>
+            <div className='reactions-div'>
+              <img className='reaction' src={link}></img>
+              <img className='reaction' src={favorite}></img>
+            </div>
           </div>
 
           <div className="post--container">
             <Quotations numberOfQuotes={1}/>
+            <div className='reactions-div'>
+              <img className='reaction' src={link}></img>
+              <img className='reaction' src={favorite}></img>
+            </div>
           </div>
 
           <div className="post--container">
             {/* <Aesthetic  numberOfImages={2}/> */}
             <Images numberOfImages={3}/>
+            <div className='reactions-div'>
+              <img className='reaction' src={link}></img>
+              <img className='reaction' src={favorite}></img>
+            </div>
           </div>
 
           <div className="post--container">
             <Quotations numberOfQuotes={1}/>
+            <div className='reactions-div'>
+              <img className='reaction' src={link}></img>
+              <img className='reaction' src={favorite}></img>
+            </div>
           </div>
 
           <div className="post--container">
             <Images numberOfImages={1}/>
             <Quotations numberOfQuotes={1}/>
+            <div className='reactions-div'>
+              <img className='reaction' src={link}></img>
+              <img className='reaction' src={favorite}></img>
+            </div>
           </div>
 
           <div className="post--container">
           <AestheticPicture  numberOfPictures={12}/>
           <Quotations numberOfQuotes={1}/>
+          <div className='reactions-div'>
+              <img className='reaction' src={link}></img>
+              <img className='reaction' src={favorite}></img>
+            </div>
           </div>
       </div>
 
