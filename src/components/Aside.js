9b91bd5cb23f5cd4b6b708538a83fcda727f300b
ChahrazadBouragba <chbouragba25@gmail.com>
2023-06-16 19:08:08 +0100

    added interaction imgs

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -11,10 +11,12 @@ export default function Aside() {
                     <p className='profile-tags'>ko-fi &#x2022; lit &#x2022; words &#x2022; photos &bull; still</p>
                     <p>This blog is a public display of art works</p>
                 </div>
+                <form>
                 <button>Home</button>
                 <button>Ask</button>
                 <button>Tags</button>
                 <button>link</button>
+                </form>
             </div>
         </aside>
     )
